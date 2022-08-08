e14c9652315b78f0572dac9ab63cadb8ef6a2390
VivekRana <vivekranarajput@gmail.com>
2022-08-08 10:58:05 -0400

    second commit js included, logic works

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 /*----- constants -----*/
+const color = ["red", "blue", "yellow", "green"]
 /*----- app's state (variables) -----*/
 let computerTurn;
 let playerTurn;
 let yourScore;
 let highScore;
 let message;
-let playerSeq;
-let computerSeq;
-let color;
+let playerSeq = [];
+let computerSeq = [];
+let randomNum
+
+
 /*----- cached element references -----*/
 let playerEl = document.querySelectorAll(".btn")
 
@@ -16,7 +19,16 @@ let playerEl = document.querySelectorAll(".btn")
 playerEl.forEach(function(e){
     e.addEventListener("click", getElement)
 })
+document.addEventListener("keypress", keyboard)
 /*----- functions -----*/
+
+
+function computerFn(){
+    randomNum = Math.floor(Math.random() * 4)
+    computerSeq.push(color[randomNum])
+    animation(color[randomNum])
+}
+
 init()
 
 function init() {
@@ -25,8 +37,10 @@ function init() {
   yourScore = 0
   highScore = 0
   message = "Press any key to start the game"
-  playerSeq = [];
-  computerSeq = [];
+  randomNum = 0;
+  
+  
+
 
 //   render()
 }
@@ -34,14 +48,34 @@ function init() {
 function getElement(){
     playerChoice = this.getAttribute("id")
     playerSeq.push(playerChoice)
-    buttonPressed(playerChoice);
+    animation(playerChoice);
+    compareChoices();
+
 }
 
-function buttonPressed(color){
-    console.log(color)
+function animation(color){
     document.getElementById(color).classList.add("active")
     
     setTimeout(function(){
         document.getElementById(color).classList.remove("active")
-    }, 200)
+    }, 500)
+}
+
+function keyboard(){
+    computerFn()
+}
+
+function compareChoices(){
+    
+    if(playerSeq[playerSeq.length - 1] === computerSeq[playerSeq.length - 1]){
+        if(playerSeq.length === computerSeq.length){
+            setTimeout(function(){
+                computerFn()
+                playerSeq = [];
+                
+            },1000)
+        }
+    }else{
+        console.log("Game over")
+    }
 }
